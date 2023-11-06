bc82b38628f197e9fe4b521ae45bc1893265a3af
ashWhiteHat <phantomofrotten@gmail.com>
2023-11-06 20:30:35 +0900

    secp224k1: set order

--- k224/src/lib.rs
+++ k224/src/lib.rs
@@ -25,3 +25,57 @@
 //! - [`ecdsa::VerifyingKey`]
 //!
 //! Please see type-specific documentation for more information.
+
+pub use elliptic_curve;
+
+use elliptic_curve::{
+    consts::{U28, U29},
+    generic_array::GenericArray,
+    FieldBytesEncoding,
+};
+
+#[cfg(target_pointer_width = "32")]
+pub use elliptic_curve::bigint::U224 as Uint;
+
+#[cfg(target_pointer_width = "64")]
+use elliptic_curve::bigint::U256 as Uint;
+
+/// Order of the secp224k1 elliptic curve in hexadecimal.
+#[cfg(any(target_pointer_width = "32", feature = "arithmetic"))]
+const ORDER_HEX: &str = "10000000000000000000000000001DCE8D2EC6184CAF0A971769FB1F7";
+
+/// secp224k1 (K-224) elliptic curve.
+#[derive(Copy, Clone, Debug, Default, Eq, PartialEq, PartialOrd, Ord)]
+pub struct Secp224k1;
+
+impl elliptic_curve::Curve for Secp224k1 {
+    /// 28-byte serialized field elements.
+    type FieldBytesSize = U28;
+
+    /// Big integer type used for representing field elements.
+    type Uint = Uint;
+
+    /// Order of NIST P-224's elliptic curve group (i.e. scalar modulus).
+    #[cfg(target_pointer_width = "32")]
+    const ORDER: Uint = Uint::from_be_hex(ORDER_HEX);
+
+    /// Order of NIST P-224's elliptic curve group (i.e. scalar modulus).
+    #[cfg(target_pointer_width = "64")]
+    const ORDER: Uint =
+        Uint::from_be_hex("000000010000000000000000000000000001DCE8D2EC6184CAF0A971769FB1F7");
+}
+
+impl elliptic_curve::PrimeCurve for Secp224k1 {}
+
+/// Compressed SEC1-encoded secp224k1 (k-224) curve point.
+pub type CompressedPoint = GenericArray<u8, U29>;
+
+/// secp224k1 (K-224) field element serialized as bytes.
+///
+/// Byte array containing a serialized field element value (base field or scalar).
+pub type FieldBytes = elliptic_curve::FieldBytes<Secp224k1>;
+
+impl FieldBytesEncoding<Secp224k1> for Uint {}
+
+/// secp224k1 (K-224) secret key.
+pub type SecretKey = elliptic_curve::SecretKey<Secp224k1>;
